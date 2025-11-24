461635f1e5436b3d7b66324fab616d2283d990d9
4tunexx <airijuz@gmail.com>
2025-11-24 15:36:36 +0000

    Update project files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL,
+    WS_URL: process.env.WS_URL,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  },
 };
 
-module.exports = nextConfig;\n
+module.exports = nextConfig;
